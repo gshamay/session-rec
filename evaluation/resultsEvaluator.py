706497095148db2ef5a4b923e8357d28a643a563
gshamay <gshamay@gmail.com>
2022-02-28 01:46:19 +0200

    Update resultsEvaluator.py

--- evaluation/resultsEvaluator.py
+++ evaluation/resultsEvaluator.py
@@ -4,6 +4,7 @@ from sklearn.metrics import precision_recall_curve
 import sys
 import matplotlib.pyplot as plt
 
+
 # parameters:
 # 1. resuls directory - resultsFile contain 'clfProbs.csv' and 'clfProbsBaseLine.csv'
 # for example C:\bgu\session-rec\finalResults\rsc15_64_1EOS_LR\SGNN\
@@ -20,10 +21,10 @@ import matplotlib.pyplot as plt
 
 def perRec(Y, results):
     # todo: merge a few graphs 1 - 10 - 100
-    res =  results.values.flatten().tolist()
-    precision, recall, thresholds = precision_recall_curve(Y,res)
-    #thresholds = [0.2,0.25,0.3,0.35]
-    #classificationPerTresholdEvaluation(Y, results, precision, recall, thresholds)
+    res = results.values.flatten().tolist()
+    precision, recall, thresholds = precision_recall_curve(Y, res)
+    # thresholds = [0.2,0.25,0.3,0.35]
+    # classificationPerTresholdEvaluation(Y, results, precision, recall, thresholds)
     return precision, recall, thresholds
 
 
@@ -107,21 +108,62 @@ def calcTPTNFPFN(Y, results, t):
 
 
 resultsFilesDir = []
+names = []
 dataLocation = None
-testFileReady = False
-if (len(sys.argv) <= 1):
-    # hard coded test values
-    #resultsFilesDir.append("C:/bgu/session-rec/results/diginetica/diginetica_Short_1EOS_LR/AR/")
-    resultsFilesDir.append("C:/bgu/session-rec/finalResults/rsc15_64_1EOS_LR/SGNN/")
-    # resultsFilesDir.append("C:/bgu/session-rec/finalResults/rsc15_64_10EOS_LR/SGNN/")
-    # test data for 1-10 or 100 EOS can be similare
+testFileReady = True
+fileName = ''
 
-    #dataLocation = 'C:/bgu/session-rec/data/diginetica/prepared/diginetica_1EOS_short/train-item-views_full_test.txt'
+def rscAll():
+    resultsFilesDir = []
+    names = []
+    resultsFilesDir.append("C:/bgu/session-rec/finalResults/rsc15_64_1EOS_LR/CSRM/")
+    names.append('CSRM')
+    resultsFilesDir.append("C:/bgu/session-rec/finalResults/rsc15_64_1EOS_LR/SGNN/")
+    names.append('SR-GNN')
+    resultsFilesDir.append("C:/bgu/session-rec/finalResults/rsc15_64_1EOS_LR/VSKNN/")
+    names.append('VSKNN')
+    fileName = 'rsc_1EOS_all'
+
+
+def rscSGNN():
+    resultsFilesDir = []
+    names = []
+    resultsFilesDir.append("C:/bgu/session-rec/finalResults/rsc15_64_1EOS_LR/SGNN/")
+    names.append('1 aEOS')
+    resultsFilesDir.append("C:/bgu/session-rec/finalResults/rsc15_64_10EOS_LR/SGNN/")
+    names.append('10 aEOS')
+    resultsFilesDir.append("C:/bgu/session-rec/finalResults/rsc15_64_100EOS_LR/SGNN/")
+    names.append('100 aEOS')
+    fileName = 'rsc_1-10-100-gnn'
+
+if (len(sys.argv) <= 1):
+    ######################################################
+    # CLASSIFIERS
+    # hard coded test value
+
+    # rscAll()
+    rscSGNN()
+
+    # resultsFilesDir.append("C:/bgu/session-rec/finalResults/diginetica_1EOS_LR/SGNN/")
+    # resultsFilesDir.append("C:/bgu/session-rec/finalResults/diginetica_10EOS_LR/SGNN/")
+    # resultsFilesDir.append("C:/bgu/session-rec/finalResults/diginetica_100EOS_LR/SGNN/")
+
+    # resultsFilesDir.append("C:/bgu/session-rec/finalResults/retailrocket_1EOS_W5_LR/SGNN/")
+
+    # DATA
+    # test data for 1-10 or 100 EOS can be similare
     dataLocation = 'C:/bgu/session-rec/data/rsc15/prepared/rsc15_64_1EOS/rsc15-clicks64_test.txt'
-    # dataLocation  = 'C:/bgu/session-recdata/diginetica/prepared/diginetica_1EOS/train-item-views_full_test.txt'
-    testFileReady = False
+
+    # dataLocation  = 'C:/bgu/session-rec/data/diginetica/prepared/diginetica_1EOS/train-item-views_full_test.txt'
+
+    # dataLocation = 'C:/bgu/session-rec/data/retailrocket/slices/1EOS/events_test.4.txt'
+
+    ###################################
+    # Debug - Short Files
+    # resultsFilesDir.append("C:/bgu/session-rec/results/diginetica/diginetica_Short_1EOS_LR/AR/")
+    # dataLocation = 'C:/bgu/session-rec/data/diginetica/prepared/diginetica_1EOS_short/train-item-views_full_test.txt'
 else:
-    # Read values from teh cmd
+    # Read values from the cmd
     # parser = argparse.ArgumentParser(description='Process some integers.')
     # parser.add_argument('-l','--list', nargs='+', help='<Required> Set flag', required=True)
     # Use like: # python arg.py -l 1234 2345 3456 4567
@@ -131,12 +173,18 @@ else:
     dataLocation = sys.argv[2]
     testFileReady = False
 
+print(resultsFilesDir)
+print(dataLocation)
 if testFileReady:
     testData = pd.read_csv(dataLocation + '.Y.csv', sep='\t', header=None)
 else:
     testData = pd.read_csv(dataLocation, sep='\t', dtype={'ItemId': np.int64})
+    session_key = 'SessionId'
+    time_key = 'Time'
+    testData.sort_values([session_key, time_key], inplace=True)
     countSessionsInTest = testData.groupby(['SessionId']).count()
     sessions = testData.groupby(['SessionId']).count()
+
     # if len(testData) - len(sessions) != len(results):
     #     print('Error - data Lens not fit;  test' + str(len(testData)) + ' sessions ' + str(len(sessions)) + ' = ' + str(len(testData) - len(sessions)) + ' != ' + str(len(results)))
     #     exit(0)
@@ -167,6 +215,14 @@ plt.figure(0, clear=True)
 Y = testData.values.flatten().tolist()
 # Y = list(reversed(Y))
 
+plotStyle = [
+    '-',  # solid line style
+    '--',  # dashed line style
+    '-.',  # dash-dot line style
+    ':'  # dotted line style
+]
+plotStyleIdx = 0
+
 bBaseLineEvaluadted = False
 if not bBaseLineEvaluadted:
     resDir = resultsFilesDir[0]
@@ -175,14 +231,64 @@ if not bBaseLineEvaluadted:
     print('resultsBL')
     bBaseLineEvaluadted = True
     precision, recall, thresholds = perRec(Y, resultsBL)
-    plt.plot(recall, precision, label="Baseline")
 
+    while (len(thresholds) < len(precision)):
+        precision = np.delete(precision, len(precision) - 1)
+
+    while (len(thresholds) < len(recall)):
+        recall = np.delete(recall, len(recall) - 1)
+
+    while (len(thresholds) > len(precision)):
+        thresholds = np.delete(thresholds, len(thresholds) - 1)
+
+    plt.figure(0, clear=True)
+    plt.plot(thresholds, precision, label="precision")
+    plt.plot(thresholds, recall, label="recall")
+    plt.xlabel('Threshold')
+    plt.ylabel('Precision,Recall')
+    plt.legend()
+    plt.savefig(dataLocation + ' BaseLineThresholds.png')
+
+    limit = 0.00
+    precision = precision[recall > limit]
+    thresholds = thresholds[recall > limit]
+    recall = recall[recall > limit]
+    # limit = 1.00
+    # precision = precision[recall < limit]
+    # recall = recall[recall < limit]
+    # recall = recall[precision < limit]
+    # precision = precision[precision < limit]
+
+    plt.figure(1, clear=True)
+    plt.plot(recall, precision, plotStyle[plotStyleIdx], label="Baseline")
+    plotStyleIdx += 1
+    plotStyleIdx = plotStyleIdx % len(plotStyle)
+
+namesIdx = 0
 for resDir in resultsFilesDir:
     resultsFile = resDir + 'clfProbs.csv'  # the current classifier
     results = pd.read_csv(resultsFile, sep='\t', header=None)
     precision, recall, thresholds = perRec(Y, results)
-    ################################################
-    plt.plot(recall, precision, label="Model")
+
+    # limit = 0.00
+    # precision = precision[recall > limit]
+    # recall = recall[recall > limit]
+    # recall = recall[precision > limit]
+    # precision = precision[precision > limit]
+    #
+    # limit = 1.00
+    # precision = precision[recall < limit]
+    # recall = recall[recall < limit]
+    # recall = recall[precision < limit]
+    # precision = precision[precision < limit]
+
+    plt.plot(recall, precision, plotStyle[plotStyleIdx], label=names[namesIdx])
+    namesIdx += 1
+    plotStyleIdx += 1
+    plotStyleIdx = plotStyleIdx % len(plotStyle)
+
+# ZOOM
+# plt.axis([0, None, 0, max(precision)])  # plt.axis([x_min, x_max, y_min, y_max])
 
 plt.xlabel('Recall')
 plt.ylabel('Precision')
